e837ce9e05c9c9d6ccfbcf1be9f66945da412a60
Pedro Almeida <p.almeida@live.com>
2022-05-24 09:12:38 -0400

    Avancando nas tentativas

--- C-II_Avancando_na_linguagem/index.c
+++ C-II_Avancando_na_linguagem/index.c
@@ -6,22 +6,38 @@ int main() {
     char palavraSecreta[20];
 
     sprintf(palavraSecreta,"MELANCIA");
-    printf("%s\n",palavraSecreta);
-    
     int acertou = 0;
-    int enforcou = 1;
+    int enforcou = 0;
     char chute;
+    char chutes[26];
+    int tentativas = 0;
 
     do{
-        printf("Digite o seu chute:");        
-        scanf("%c", &chute);
-
         for (int i = 0; i < strlen(palavraSecreta); i++){
-            if(palavraSecreta[i] == chute){
-                printf("A posição %d tem essa letra!\n", i+1);
+                
+                int achou = 0;
+
+                for (int j = 0; j < tentativas; j++){
+                        if(chutes[j] == palavraSecreta[i]){
+                        achou = 1;
+                        break;
+                    }
+                }
+            if (achou){
+                printf("%c ", palavraSecreta[i]);
+            }else{
+                printf("_ ");
             }
         }
-    }while(!acertou && !enforcou);
+       printf("\n");
+
+       printf("Digite o seu chute:");        
+       scanf(" %c", &chute);
+
+        chutes[tentativas] = chute;
+        tentativas++;
+
+    }while(!acertou || !enforcou);
 
 
 
