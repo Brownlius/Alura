3d8c0a8baabf599c928d3d6ea14cc27db7072cd5
Pedro Almeida <p.almeida@live.com>
2022-05-24 08:46:29 -0400

    Percorrendo o array

--- C-II_Avancando_na_linguagem/index.c
+++ C-II_Avancando_na_linguagem/index.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
     char palavraSecreta[20];
 
     sprintf(palavraSecreta,"MELANCIA");
+    printf("%s\n",palavraSecreta);
     
-    
-    do{
+    int acertou = 0;
+    int enforcou = 1;
+    char chute;
 
+    do{
+        printf("Digite o seu chute:");        
+        scanf("%c", &chute);
+
+        for (int i = 0; i < strlen(palavraSecreta); i++){
+            if(palavraSecreta[i] == chute){
+                printf("A posição %d tem essa letra!\n", i+1);
+            }
+        }
     }while(!acertou && !enforcou);
 
 
