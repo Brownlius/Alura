ffcb3ea74280c1d13602c36cfa4b95c2d039f198
Pedro Almeida <p.almeida@live.com>
2022-05-24 10:23:31 -0400

    adicionando funcao e parâmetro

--- C-II_Avancando_na_linguagem/index.c
+++ C-II_Avancando_na_linguagem/index.c
@@ -8,6 +8,14 @@ void abertura(){
     printf("***********************\n\n");
 }
 
+void chuta(char chutesFuncao[26], int tentativasFuncao){
+    char chute;
+    printf("Digite o seu chute:");        
+       scanf(" %c", &chute);
+
+        chutesFuncao[tentativasFuncao] = chute;
+}
+
 int main() {
 
     abertura();
@@ -17,15 +25,13 @@ int main() {
     sprintf(palavraSecreta,"MELANCIA");
     int acertou = 0;
     int enforcou = 0;
-    char chute;
     char chutes[26];
     int tentativas = 0;
+    
 
     do{
         for (int i = 0; i < strlen(palavraSecreta); i++){
-                
                 int achou = 0;
-
                 for (int j = 0; j < tentativas; j++){
                         if(chutes[j] == palavraSecreta[i]){
                         achou = 1;
@@ -40,12 +46,9 @@ int main() {
         }
        printf("\n");
 
-       printf("Digite o seu chute:");        
-       scanf(" %c", &chute);
-
-        chutes[tentativas] = chute;
+       chuta(chutes, tentativas);
         tentativas++;
-
+        
     }while(!acertou || !enforcou);
 
 
