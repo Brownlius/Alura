fbd6d6a991d34e80eb8a62042806b0e7e7245ed3
Pedro Almeida <p.almeida@live.com>
2022-05-24 08:32:22 -0400

    Iniciando o  jogo da forca

---
+++ C-II_Avancando_na_linguagem/index.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+
+    char palavraSecreta[20];
+
+    sprintf(palavraSecreta,"MELANCIA");
+    
+    
+    do{
+
+    }while(!acertou && !enforcou);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
