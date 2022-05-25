4e6d7b6cba1f0f5e7f32b3108bbf9e82f5fdf135
Pedro Almeida <p.almeida@live.com>
2022-05-25 08:36:50 -0400

    Imprimindo matriz - PacMan

--- C-III_recursos_avancados_da_linguagem/index.c
+++ C-III_recursos_avancados_da_linguagem/index.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-#include <stdlib.h> 
-#include "index.h"
+#include <stdlib.h>
 
 int main(){
 
+    char mapa[5][10 + 1];
+
+    FILE* f;
+    f = fopen("mapa.txt", "r");
+
+    if(f == 0){
+        printf("Arquivo não encontrado!");
+        exit(1);
+    }
+    for (int i = 0; i < 5; i++){
+        fscanf(f, "%s", mapa[i]);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n",mapa[i]);
+    }    
+    
 
 
-
-
+    fclose(f);
 
 
 
@@ -21,4 +37,5 @@ int main(){
 
 
     return 0;
+
 }
