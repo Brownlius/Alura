cf9bec4cdf4bf9a321597a2d7d93d44ee7943eec
Pedro Almeida <p.almeida@live.com>
2022-06-03 10:08:55 -0400

    Acionado dificuldade e pontos

--- C++-Conhecendo_linguagem_e_STL/adivinha.cpp
+++ C++-Conhecendo_linguagem_e_STL/adivinha.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+const int NUMERO_SECRETO = 48;
+
 using namespace std;
 
 int main(){
@@ -7,11 +9,57 @@ int main(){
     cout << "#Bem-vindo ao jogo da advinhação!#"<< endl;
     cout << "##################################" << endl;
 
-    int numeroSecreto = 48;
+    char dificuldade;
+    int chances;
 
-    cout << "O número secreto é "<< numeroSecreto <<", não conte para ninguém!" << endl;
+    cout << "Escolha sua dificuldade: (F)Fácil, (M)Médio ou (D)Difícil." << endl;
+    cin >> dificuldade;
 
+    switch (dificuldade){
+    case 'F':
+        chances = 10;
+        break;
+    case 'M':
+        chances = 6;
+        break;
+    case 'D':
+        chances = 3;
+        break;
+    }
+    int pontos = 100;
+    int tentativas;
+    double pontos_perdidos;
+    for (int i = 1; i <= chances; i++){
 
+        int chute;
+        tentativas++;
+        
+        cout << "Qual o seu chute? " << endl;
+        cin >> chute;
+        
+        pontos_perdidos += abs(chute - NUMERO_SECRETO)/2.0;
+        
+        bool acertou = (chute == NUMERO_SECRETO);
+        bool chuteMaior = (chute > NUMERO_SECRETO);
+        
+        if(acertou){
+            cout << "Parabéns. Tu é o brabo." << endl;        
+            cout << "Você acertou em " << tentativas << "tentativas." << endl;
+            double pontos_final = (pontos - pontos_perdidos);  
+            cout.precision(2);
+            cout << fixed;
+            cout << "Sua pontuação foi: "<< pontos_final <<"." << endl;
+            break;
+        }else if(chuteMaior){
+            cout << "Infelizmente não tens o que é necessário." << endl;
+            cout << "o número correto é menor." << endl;
+        }else{
+            cout << "Infelizmente não tens o que é necessário." << endl;
+            cout << "o número correto é maior." << endl;
+        }
+    }
+    
+    
 
 
 
