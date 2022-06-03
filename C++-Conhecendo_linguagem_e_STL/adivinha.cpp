8c0c9a2b2fb8b77aa7cc5bfa6e0d87af22b4d345
Pedro Almeida <p.almeida@live.com>
2022-06-03 08:33:57 -0400

    introucao a cout

---
+++ C++-Conhecendo_linguagem_e_STL/adivinha.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout << "##################################" << endl;
+    cout << "#Bem-vindo ao jogo da advinhação!#"<< endl;
+    cout << "##################################" << endl;
+
+    int numeroSecreto = 48;
+
+    cout << "O número secreto é "<< numeroSecreto <<", não conte para ninguém!" << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
