5873ca3134c85e759c0def7ad6bbd1e707b8e109
Pedro Almeida <p.almeida@live.com>
2022-06-06 09:44:27 -0400

    Finalizado primeiro crso

--- C++-Conhecendo_linguagem_e_STL/forca/forca.cpp
+++ C++-Conhecendo_linguagem_e_STL/forca/forca.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <string>
-#include<map>
-#include<vector>
+#include <map>
+#include <vector>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
-const string PALAVRA_SECRETA = "MELANCIA";
+string palavra_secreta; 
 map<char, bool> chutou;
-vector<char>chutes_errados;
+vector<char> chutes_errados;
 
-bool existe_letra(char chute){
-    for(char letra : PALAVRA_SECRETA){
+bool letra_existe(char chute){
+    for(char letra : palavra_secreta){
         if(chute == letra){
-            return 1;
+            return true;
         }
     }
-    return 0;
+    return false;
 }
 
 bool nao_acertou(){
-    for(char letra : PALAVRA_SECRETA){
+    for(char letra : palavra_secreta){
         if(!chutou[letra]){
             return true;
         }
     }
-        return false;
+    return false;
 }
 
 bool nao_enforcou(){
@@ -31,10 +34,9 @@ bool nao_enforcou(){
 }
 
 void imprime_cabeca(){
-    cout << "************************" <<endl;
-    cout << "**JOGO DO ENFORCAMENTO**" <<endl;
-    cout << "************************" <<endl;
-    cout << endl;
+    cout << "*********************" << endl;
+    cout << "*** Jogo da Forca ***" << endl;
+    cout << "*********************" << endl;
     cout << endl;
 }
 
@@ -47,50 +49,124 @@ void imprime_erros(){
 }
 
 void imprime_palavra(){
-    for(char letra : PALAVRA_SECRETA){
-            if (chutou [letra]){
-                cout << letra << " ";
-            }
-            else{
-                cout << "_ ";
-            }
+    for(char letra : palavra_secreta){
+        if(chutou[letra]){
+            cout << letra << " ";
         }
-        cout << endl;
+        else{
+            cout << "_ ";
+        }
+    }
+    cout << endl;
 }
 
 void chuta(){
-    cout << "Digite seu chute: " ;
+    cout << "Seu chute: ";
     char chute;
     cin >> chute;
+
     chutou[chute] = true;
 
-    if(existe_letra){
-        cout << "Acertou" <<endl;
-    }else{
+    if(letra_existe(chute)){
+        cout << "Você acertou! Seu chute está na palavra." << endl;
+    }
+    else{
+        cout << "Você errou! Seu chute não está na palavra." << endl;
         chutes_errados.push_back(chute);
     }
-    
     cout << endl;
 }
-int main(){
 
-    imprime_cabeca();
-    
-    bool acertou = false;
-    bool enforcou = false;
+vector<string> le_arquivo(){
+    ifstream arquivo;
+    arquivo.open("palavras.txt");
+
+    if(arquivo.is_open()){
+        int quantidade_palavras;
+        arquivo >> quantidade_palavras;
+
+
+        vector<string> palavras_do_arquivo;
+
+        for(int i=0;i<quantidade_palavras;i++){
+            string palavra_lida;
+            arquivo >> palavra_lida;
+            palavras_do_arquivo.push_back(palavra_lida);
+        }
+
+        arquivo.close();
+        return palavras_do_arquivo;
+    }
+    else{
+        cout << "Não foi possível acessar o banco de palavras." << endl;
+        exit(0);
+    }
+}
+
+void sorteia_palavra(){
+    vector<string> palavras = le_arquivo();
+
+    srand(time(NULL));
+    int indice_sorteado = rand() % palavras.size();
+
+    palavra_secreta = palavras[indice_sorteado];
+}
+
+void salva_arquivo(vector<string> nova_lista){
+    ofstream arquivo;
+    arquivo.open("palavras.txt");
+    if(arquivo.is_open()){
+        arquivo << nova_lista.size() << endl;
+
+        for(string palavra : nova_lista){
+            arquivo << palavra << endl;
+        }
+        arquivo.close();
+    }
+    else{
+        cout << "Não foi possível acessar o banco de palavras." << endl;
+        exit(0);
+    }
+}
+
+void adiciona_palavra(){
+    cout << "Digite a nova palavra, usando letras maiúsculas." << endl;
+    string nova_palavra;
+    cin >> nova_palavra;
+
+    vector<string> lista_palavras = le_arquivo();
+    lista_palavras.push_back(nova_palavra);
+
+    salva_arquivo(lista_palavras);
+}
+
+int main () {
+    imprime_cabecalho();
+
+    le_arquivo();
+    sorteia_palavra();
 
     while(nao_acertou() && nao_enforcou()){
         imprime_erros();
-        imprime_palavra();
-        chuta();
 
+        imprime_palavra();
+
+        chuta();
     }
+
     cout << "Fim de jogo!" << endl;
-    cout << "A palavra secreta era: " << PALAVRA_SECRETA <<endl;
+    cout << "A palavra secreta era: " << palavra_secreta << endl;
     if(nao_acertou()){
         cout << "Você perdeu! Tente novamente!" << endl;
-    }else{ 
-        cout << "Parabéns! Você acertou a palavra secreta!" << endl;
     }
-    
+    else{
+        cout << "Parabéns! Você acertou a palavra secreta!" << endl;
+
+        cout << "Você deseja adicionar uma nova palavra ao banco? (S/N) ";
+        char resposta;
+        cin >> resposta;
+        if(resposta == 'S'){
+            adiciona_palavra();
+        }
+    }
 }
