209370d6fa777129b88c0b70ab2924726e97979c
Pedro Almeida <p.almeida@live.com>
2022-09-16 12:29:22 -0400

    continuando Boas_praticas_arq

--- Angular-Boas_praticas_arq&forms/CatBook/src/app/home/home.module.ts
+++ Angular-Boas_praticas_arq&forms/CatBook/src/app/home/home.module.ts
@@ -12,6 +12,9 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     HomeRoutingModule
+  ],
+  exports: [
+    HomeComponent
   ]
 })
 export class HomeModule { }
