0327a35d976ca515c87b83af5d1f59ee1ca5232e
Pedro Almeida <p.almeida@live.com>
2022-09-19 16:54:49 -0400

    .

--- Angular-Boas_praticas_arq&forms/CatBook/src/app/app.module.ts
+++ Angular-Boas_praticas_arq&forms/CatBook/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,9 +12,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:[AppComponent]
 })
 export class AppModule { }
--- Angular-Boas_praticas_arq&forms/CatBook/src/app/home/home.module.ts
+++ Angular-Boas_praticas_arq&forms/CatBook/src/app/home/home.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
-
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    FormsModule
+    FormsModule,
+    MatIconModule,
+    
   ],
   exports: [
     HomeComponent
